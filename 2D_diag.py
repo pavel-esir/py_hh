8ec214490a7dbbe44daf63b8cf21670719d66be9
Pavel Esir <esirpavel@gmail.com>
2016-10-17 14:15:02 +0300

    Corrected calculating 2d phase diagramm

--- 2D_diag.py
+++ 2D_diag.py
@@ -10,12 +10,13 @@ import numpy as np
 import matplotlib.pylab as pl
 np.random.seed(0)
 
-Ntrans = 1 # length of transient process in periods, integer
-T = 20.275
+Ntrans = 6 # length of transient process in periods, integer
+#T = 20.275
+T = 20.28
 SimTime = (Ntrans+0.5)*T
-h = 0.02
+h = 0.01
 Tsim = int(SimTime/h)
-recInt = 1
+recInt = 1000
 
 tau_psc = 0.2  # ms
 w_p = 0.0      # Poisson noise weith, pA
@@ -23,7 +24,7 @@ w_n = 0.0      # connection weight, pA
 wInc = 1.3
 
 rate = 0.1     # Poisson noise rate, Hz (shouldn't  be 0)
-dT = 0.5
+dT = 0.04
 Nneur = int(T/dT)**2
 Ncon = 1
 pre = np.random.randint(0, Nneur, Ncon).astype('uint32')
@@ -54,6 +55,7 @@ dts = np.linspace(0, int(T/h), int(np.sqrt(Nneur)), endpoint=False, dtype='uint3
 t1, t2 = np.meshgrid(dts, dts)
 incTimes[0, :] = t1.reshape(Nneur)
 incTimes[1, :] = t2.reshape(Nneur)
+incTimes = np.sort(incTimes, axis=0)
 
 setIncomSpikes(incTimes, nums, incSpWeights, NnumSp)
 #%%
@@ -71,6 +73,7 @@ setSpikeTimes(spike_times, num_spikes_neur, np.shape(spike_times)[0]*Nneur)
 setConns(weight, delay,  pre, post)
 #%%
 simulate()
+#np.save('spikes_T_{}_h_{}_dT_{}_Ntrans_{}'.format(T, h, dT, Ntrans), (spike_times))
 #%%
 #pl.figure()
 #pl.plot(np.linspace(0, SimTime, int((Tsim + recInt - 1)/recInt)), Vrec[:, :100:10])
@@ -79,22 +82,24 @@ simulate()
 #pl.show()
 #pl.xlim((0, SimTime))
 #%%
-pl.figure(1)
-lastSpikeTime = np.zeros(Nneur)
-for i, n in enumerate(num_spikes_neur):
-    if n < Ntrans:
-            lastSpikeTime[i] = np.nan
-    else:
-        lastSpikeTime[i] = spike_times[Ntrans - 1, i]*h
-
-lastSpikeTime = lastSpikeTime.reshape(np.sqrt(Nneur), np.sqrt(Nneur))
-mArr = np.ma.array(lastSpikeTime, mask=(lastSpikeTime != lastSpikeTime))
-
-pl.pcolormesh(dts*h, dts*h, mArr, cmap='jet')
-pl.colorbar()
-
+#pl.figure(1)
+#lastSpikeTime = np.zeros(Nneur)
+#for i, n in enumerate(num_spikes_neur):
+#    if n < Ntrans:
+#            lastSpikeTime[i] = np.nan
+#    else:
+#        lastSpikeTime[i] = spike_times[Ntrans - 1, i]*h
+#
+#lastSpikeTime = lastSpikeTime.reshape(np.sqrt(Nneur), np.sqrt(Nneur))
+#mArr = np.ma.array(lastSpikeTime, mask=(lastSpikeTime != lastSpikeTime))
+#
+#pl.pcolormesh(dts*h, dts*h, mArr, cmap='jet')
+#pl.colorbar()
+#pl.xlim((0, T))
+#pl.ylim((0, T))
+#
 #pl.plot(dts*h, np.ma.array((Ntrans*T + h*dts) - h*lastSpikeTime, mask=(lastSpikeTime != lastSpikeTime)), '-')
-#pl.plot(arange(0, T, 0.01), arange(0, T, 0.01))
+##pl.plot(arange(0, T, 0.01), arange(0, T, 0.01))
 #pl.ylim((0, T))
 #pl.xlim((0, T))
 #pl.show()
